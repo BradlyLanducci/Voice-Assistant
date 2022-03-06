809305cd03713621d1783b5cc386910f82e69c1c
Bradly landucci <bradly.landucci.5149@mail.linnbenton.edu>
2022-03-06 00:13:17 -0800

    Added text to speech functionality
    As of now it only can write one file, this is due to it not being able to rewrite over the created .wav file twice. A solution is to create a new name each time.

--- Speech_Recognition.py
+++ Speech_Recognition.py
@@ -1,6 +1,11 @@
 import speech_recognition
 import pyttsx3
 
+from playsound import playsound
+from gtts import gTTS
+
+import os
+
 recognizer = speech_recognition.Recognizer()
 
 while True:
@@ -14,6 +19,17 @@ while True:
             text = recognizer.recognize_google(audio)
             text = text.lower()
 
+            voice = text
+            
+            language = 'en'
+
+            myobj = gTTS(text=voice, lang=language, slow=False)
+
+            myobj.save("welcome.mp3")
+  
+            # Playing the converted file
+            #os.system("mpg321 C:/Users/bradl/Desktop/Code Projects/Speech_Recognition/Voice-Assistant/welcome.mp3")
+            playsound('C:/Users/bradl/Desktop/Code Projects/Speech_Recognition/Voice-Assistant/welcome.mp3')
             print(f"Recognized {text}")
 
     except speech_recognition.UnknownValueError:
