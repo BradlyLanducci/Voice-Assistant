458509ca69e7c9a0d8dd2f4385a3b6a846b29a52
Bradly landucci <bradly.landucci.5149@mail.linnbenton.edu>
2022-03-07 17:00:18 -0800

    Text to speech added.
    Scrapes definitions from vocabulary.com and translates it from text to speech!

--- Speech_Recognition.py
+++ Speech_Recognition.py
@@ -9,6 +9,11 @@ from gtts import gTTS
 
 import os
 
+
+engine = pyttsx3.init() 
+
+
+
 recognizer = speech_recognition.Recognizer()
 
 while True:
@@ -17,45 +22,31 @@ while True:
         with speech_recognition.Microphone() as mic:
             recognizer.adjust_for_ambient_noise(mic, duration=0.2)
             audio = recognizer.listen(mic)
-
             text = recognizer.recognize_google(audio)
             text = text.lower()
-
             voice = text
 
             if "define" in text:
                 
                 print("Defining...")
-                
+                text = text.replace('define ', '')
                 url='https://www.vocabulary.com/dictionary/' + text
                 htmlfile = urllib.request.urlopen(url)
                 soup = BeautifulSoup(htmlfile, 'lxml')              
                 soup1 = soup.find(class_="short")
                 soup1 = soup1.get_text()
                 print(soup1)
-
+                engine.say(soup1) 
+                engine.runAndWait()
 
                 
             elif text == "how is your day going":
-                print("Good! Thanks for asking.")
+                engine.say("Good! Thanks for asking.")
+                engine.runAndWait()
             else:
+                print(text)
                 print("Invalid command...")
             
     except speech_recognition.UnknownValueError:
         recognizer = speech_recognition.Recognizer()
         continue
-
-
-# Functionality to have it talk back, currently only works by creating a file
-# The main issue with this is it needs to create a new file everytime.
-
-
-##            language = 'en'
-##
-##            myobj = gTTS(text=voice, lang=language, slow=False)
-##
-##            myobj.save("welcome.mp3")
-##  
-##            # Playing the converted file
-##            playsound('C:/Users/bradl/Desktop/Code Projects/Speech_Recognition/Voice-Assistant/welcome.mp3')
-##            print(f"Recognized {text}")
