741818dc406655110a55f83d006ff40b73c73a74
Bradly landucci <bradly.landucci.5149@mail.linnbenton.edu>
2022-03-07 18:11:03 -0800

    Exception handling for incorrect voice inputs.

--- Speech_Recognition.py
+++ Speech_Recognition.py
@@ -8,37 +8,46 @@ from bs4 import BeautifulSoup
 from playsound import playsound
 from gtts import gTTS
 
-engine = pyttsx3.init() 
+def main():      
+    engine = pyttsx3.init() 
 
-recognizer = speech_recognition.Recognizer()
+    recognizer = speech_recognition.Recognizer()
 
-while True:
-    try:
-        with speech_recognition.Microphone() as mic:
-            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-            audio = recognizer.listen(mic)
-            text = recognizer.recognize_google(audio)
-            text = text.lower()
-            voice = text
+    while True:
+        try:
+            with speech_recognition.Microphone() as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+                text = recognizer.recognize_google(audio)
+                text = text.lower()
+                voice = text
 
-            if "define" in text:
+                if "define" in text:
+                        try:
+                            print("Defining...")
+                            text = text.replace('define ', '')
+                            url='https://www.vocabulary.com/dictionary/' + text
+                            htmlfile = urllib.request.urlopen(url)
+                            soup = BeautifulSoup(htmlfile, 'lxml')              
+                            soup1 = soup.find(class_="short")
+                            soup1 = soup1.get_text()
+                            engine.say(soup1) 
+                            engine.runAndWait()
+                        except:
+                            engine.say("Define command failed. Say, define and then say a word...")
+                            engine.runAndWait()
+                            
+                elif text == "how is your day going":
+                    engine.say("Good! Thanks for asking.")
+                    engine.runAndWait()
+                    
+                else:
+                    engine.say(f"Command {text} doesn't exist.")
+                    engine.runAndWait()
                 
-                print("Defining...")
-                text = text.replace('define ', '')
-                url='https://www.vocabulary.com/dictionary/' + text
-                htmlfile = urllib.request.urlopen(url)
-                soup = BeautifulSoup(htmlfile, 'lxml')              
-                soup1 = soup.find(class_="short")
-                soup1 = soup1.get_text()
-                engine.say(soup1) 
-                engine.runAndWait()
-        
-            elif text == "how is your day going":
-                engine.say("Good! Thanks for asking.")
-                engine.runAndWait()
-            else:
-                print("Invalid command...")
-            
-    except speech_recognition.UnknownValueError:
-        recognizer = speech_recognition.Recognizer()
-        continue
+        except speech_recognition.UnknownValueError:
+            recognizer = speech_recognition.Recognizer()
+            continue
+
+if __name__ == "__main__":
+    main()
